cb58feda41569ffa578bbbdd4a76041099b026e8
Koigor97 <turaysamuel101@yahoo.com>
2024-05-02 14:48:55 +0200

    refactor create_accoutnt_type sql cript and create new sql file for assignment

---
+++ database/assignment2.sql
@@ -0,0 +1,13 @@
+-- Inserting VALUES into account table
+INSERT INTO public.account (
+	account_firstname,
+	account_lastname,
+	account_email,
+	account_password
+) VALUES (
+	'Tony',
+	'Stark',
+	'tony@starkent.com',
+	'Iam1ronM@n'
+);
+
