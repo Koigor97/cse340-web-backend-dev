4098842939056b6f1ffba55facfa60582d5bc700
Koigor97 <turaysamuel101@yahoo.com>
2024-05-02 15:16:28 +0200

    add more query statements to the sql file

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -11,3 +11,13 @@ INSERT INTO public.account (
 	'Iam1ronM@n'
 );
 
+-- Modifying account_type to 'Admin' for account_id = 1
+UPDATE public.account SET account_type = 'Admin' WHERE account_id = 1;
+
+-- Deleting account_id = 1
+DELETE FROM public.account WHERE account_id = 1;
+
+-- REPLACING part of inv_decscription text for inv_id = 10
+UPDATE public.inventory 
+	SET inv_description = REPLACE(inv_description, 'the small interiors', 'a huge interior')
+	WHERE inv_id = 10;
