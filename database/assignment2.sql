ded211743a727a14ebd9fdc3b729ec2f5c1c2812
Koigor97 <turaysamuel101@yahoo.com>
2024-05-02 16:23:16 +0200

    complete assignment1

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -21,3 +21,13 @@ DELETE FROM public.account WHERE account_id = 1;
 UPDATE public.inventory 
 	SET inv_description = REPLACE(inv_description, 'the small interiors', 'a huge interior')
 	WHERE inv_id = 10;
+
+--INNER JOIN the classification table with the inventory table
+SELECT inv_make, inv_model, classification_name FROM public.inventory 
+	INNER JOIN public.classification
+	ON inv_model = 'Sport';
+
+-- REPLACING inv_images and inv_thumbnails for inventory table
+UPDATE public.inventory
+	SET inv_image = REPLACE(inv_image, '/images', '/images/vehicles'),
+		inv_thumbnail = REPLACE(inv_thumbnail, '/images', '/images/vehicles');
