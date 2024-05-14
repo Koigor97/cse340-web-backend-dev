3e1af0ddf635efa20247b9389b954e9458868633
Koigor97 <turaysamuel101@yahoo.com>
2024-05-14 22:43:26 +0200

    modify baseController.js file

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -1,12 +1,10 @@
 const utilities = require("../utilities/");
-const { pageDescription } = require("../lib/pageMetaDesc");
 
 const baseController = {};
 
 baseController.buildHome = async function (req, res) {
-  const pageDesc = pageDescription();
   const nav = await utilities.getNav();
-  res.render("index", { title: "Home", description: pageDesc.homePage, nav });
+  res.render("index", { title: "Home", nav });
 };
 
 module.exports = baseController;
