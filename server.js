8f764b6fa369a0261e7fa4a8d63e61ef2d53759b
Koigor97 <turaysamuel101@yahoo.com>
2024-05-17 13:04:42 +0200

    require express-session package and pool into scope

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const express = require("express");
 const env = require("dotenv").config();
 const expressLayouts = require("express-ejs-layouts");
 const app = express();
+const session = require("express-session");
+const pool = require("./database/");
 
 const static = require("./routes/static");
 const baseController = require("./controllers/baseController");
@@ -21,6 +23,22 @@ const errorHandler = require("./middleware/errorHandler");
 app.set("view engine", "ejs");
 app.use(expressLayouts);
 app.set("layout", "layouts/layout");
+
+/* ***********************
+ * Middleware
+ *************************/
+app.use(
+  session({
+    store: new (require("connect-pg-simple")(session))({
+      createTableIfMissing: true,
+      pool,
+    }),
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    name: "sessionId",
+  })
+);
 
 /* ***********************
  * Routes
