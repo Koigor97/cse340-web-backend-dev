eefdd47eb4df6d93ca81ca76db06688c3fed1455
Koigor97 <turaysamuel101@yahoo.com>
2024-05-19 07:50:24 +0200

    add sanitization and validation

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const utilities = require("../utilities/");
 const errorHandlers = require("../middleware/errorHandler");
 const accountController = require("../controllers/accountController");
+const regValidate = require("../utilities/account-validation");
 
 // Route to build account view
 router.get("/login", utilities.handleError(accountController.buildLogin));
@@ -11,6 +12,8 @@ router.get("/register", utilities.handleError(accountController.buildRegister));
 // Route to process registration
 router.post(
   "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
   utilities.handleError(accountController.registerAccount)
 );
 // Error handler
